5de4b8d952c33f0aa4efef8926a34c6c8e33a089
Samuel Agbedia <solumide83@gmail.com>
2024-08-18 21:30:34 -0700

    Removed the apitests file from repository

--- apitests.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-app_id = "f1a5dd2d"
-app_key = "0dcf536829d4970fa80eb768d3421628"
-url = f"https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id={app_id}&app_key={app_key}"
-response = requests.get(url)
-print(response.text)
